140feb31b576870dc0033c4ac2343aa4a471f8a3
Kyle Adkins <kyle.m.adkins@gmail.com>
2022-05-06 02:28:14 -0500

    Download external files

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,29 @@
-import axios from 'axios'
 import styles from '../styles/Home.module.css'
 
 export async function getStaticProps() {
+  const fs = require('fs')
+  const uuid = require('uuid')
+  const http = require('http')
+
   const res = await fetch('http://localhost:8080')
   const posts = await res.json()
   const promises = posts.map((post, idx) => {
     let ext = post.image.split('.').pop()
-    ext = ext === 'svg' ? 'svg+xml' : ext
+    let filename = `${uuid.v4()}.${ext}`
+    const writer = fs.createWriteStream(`./images/${filename}`)
     // Replace localhost w/ our server
-    return axios
-      .get(`http://localhost:8080/${post.image}`, {
-        responseType: 'arraybuffer'
+    return new Promise((resolve) => {
+      http.get(`http://localhost:8080${post.image}`, function(response) {
+        response.pipe(writer);
+        writer.on("finish", () => {
+          posts[idx] = {
+            ...post,
+            image: `../images/${filename}`
+          }
+          resolve()
+        })
       })
-      .then(response => {
-        posts[idx] = {
-          ...post,
-          image: `data:image/${ext};base64,${Buffer.from(response.data, 'binary').toString('base64')}`
-        }
-      })
+    })
   })
   await Promise.all(promises)
   return {
