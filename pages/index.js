1a9edd31c41e56f14d87d316d3bd10da772343ae
Kyle Adkins <kyle.m.adkins@gmail.com>
2022-05-06 03:00:23 -0500

    Empty image directory before each new export, only download images when exporting

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
 import styles from '../styles/Home.module.css'
 
 export async function getStaticProps() {
-  const fs = require('fs')
+  const fs = require('fs-extra')
   const uuid = require('uuid')
   const http = require('http')
 
   const res = await fetch('http://localhost:8080')
   const posts = await res.json()
+
+  if (process.env.STATIC === 'false') {
+    return {
+      props: {
+        posts: posts.map(post => ({
+          ...post,
+          // Replace localhost w/ our server. Probably store in an ENV variable
+          image: `http://localhost:8080${post.image}`
+        }))
+      },
+    }
+  }
+
+  fs.emptyDirSync('./images')
+
   const promises = posts.map((post, idx) => {
     let ext = post.image.split('.').pop()
     let filename = `${uuid.v4()}.${ext}`
@@ -14,7 +29,7 @@ export async function getStaticProps() {
     // Replace localhost w/ our server
     return new Promise((resolve) => {
       http.get(`http://localhost:8080${post.image}`, function(response) {
-        response.pipe(writer);
+        response.pipe(writer)
         writer.on("finish", () => {
           posts[idx] = {
             ...post,
